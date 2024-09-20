599bf06cd0bd59fd4ea078a22b265dcb9d7b75b9
Ines Qian <inesqyx@gmail.com>
2024-09-20 06:51:26 -0400

    Implement button trigger + add an icon for place

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -19,6 +19,14 @@ const project2 = new Project(20220830, "smile", "assets/img/smile-logo.jpg", "Sm
     "A GenAI-based model that turns non-smiling pictures into smiling pictures ",
     "Developed a conditional deep convolutional generative adversarial network (cDCGAN) to generate smiling face images abnd showcased the result with Tensorboard",
     "Generated reports on analyzing the difficulty in GAN training, and received a mark of 90 under evaluation");
+const project3 = new Project(20211230, "cyber", "assets/img/cybersecurity.jpg", "Content Lead @ UofT Cyber-security Student Association", 
+    " ",
+    "Leading workshops with topics on Detecting Spam Using RNN and NLP",
+    "Composing technical writing on course selection guidelines for students who are interested in cyber-security.");
+const project4 = new Project(20211230, "veep", "assets/img/veep.png", "Full Stack Developer @ Volunteer Engineering Experience Program", 
+    " ",
+    "Built the front-end interface of the Project Page for the club using HTML, CSS, JavaScript, and ReactJS.",
+    "Collected the sign-up information of the visitors in a SQL database to track interested group.");
 
 // Javascript function: https://www.w3schools.com/js/js_functions.asp 
 function generateHTML(project){
@@ -48,7 +56,7 @@ function generateHTML(project){
           </div>`;
 }
 // Javascript arrays: https://www.w3schools.com/js/js_arrays.asp
-const projects = [project1, project2];
+const projects = [project1, project2, project3, project4];
 
 // Javascript Numeric sort: https://www.w3schools.com/js/js_array_sort.asp#mark_numeric
 // project[0] is the latest 
@@ -56,11 +64,16 @@ projects.sort(function(a, b){return b.time - a.time});
 
 document.getElementById("recent_projects").innerHTML = generateHTML(projects[0]);
 
+var load = 1;
+
 // Trigger the load more button: https://www.w3schools.com/jsref/event_onclick.asp
-function loadMore(){
+function loadMore(){ 
     // Javascript For Loop
-    for (let i = 1; i <projects.length; i++){
-        document.getElementById("recent_projects").innerHTML = generateHTML(projects[i]);
+    while (load < projects.length){
+        document.getElementById("recent_projects").innerHTML += generateHTML(projects[load]);
+        load ++;
     }
+    return;
+
 }
 
